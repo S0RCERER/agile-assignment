2aabf01e13c244d601e6c09ecb5f759886046072
Ling Feng <20099866@mail.wit.ie>
2022-11-21 01:25:43 +0000

    top-rated movies page passed test

--- cypress/e2e/base.cy.js
+++ cypress/e2e/base.cy.js
@@ -1,35 +1,35 @@
 /* eslint-disable no-undef */
-let movies; // List of movies from TMDB
-let movie; //
+let topRatedMovies;
 
-describe("Base tests", () => {
+describe("Base tests about movies lists", () => {
   before(() => {
     // Get the discover movies from TMDB and store them locally.
     cy.request(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
-        "TMDB_KEY"
-      )}&language=en-US&include_adult=false&include_video=false&page=1`
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${Cypress.env(
+      "TMDB_KEY"
+      )}&language=en-US&page=1`
     )
       .its("body") // Take the body of HTTP response from TMDB
       .then((response) => {
-        movies = response.results;
+        topRatedMovies = response.results;
       });
   });
-  beforeEach(() => {
-    cy.visit("/");
-  });
 
-  describe("The Discover Movies page", () => {
+  describe("Top Rated Movies Page", () => {
+    beforeEach(()=>{
+      cy.visit("/movies/top-rated")
+    })
+
     it("displays the page header and 20 movies", () => {
-      cy.get("h3").contains("Discover Movies");
+      cy.get("h3").contains("Top Rated Movies");
       cy.get(".MuiCardHeader-root").should("have.length", 20);
     });
 
     it("displays the correct movie titles", () => {
       cy.get(".MuiCardHeader-content").each(($card, index) => {
-        cy.wrap($card).find("p").contains(movies[index].title);
+        cy.wrap($card).find("p").contains(topRatedMovies[index].title);
       });
     });
-    
   });
+
 });
