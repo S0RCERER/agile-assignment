ea5ea2af75a3307329e83d083323187e3455f232
Ling Feng <20099866@mail.wit.ie>
2022-11-21 02:03:02 +0000

    upcoming movies page passed test

--- cypress/e2e/base.cy.js
+++ cypress/e2e/base.cy.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 let topRatedMovies;
+let upComingMovies;
 
 describe("Base tests about movies lists", () => {
   before(() => {
@@ -13,7 +14,17 @@ describe("Base tests about movies lists", () => {
       .then((response) => {
         topRatedMovies = response.results;
       });
+    cy.request(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${Cypress.env(
+      "TMDB_KEY"
+      )}&include_adult=false&language=en-US&page=1`
+    )
+      .its("body")
+      .then((response) => {
+        upComingMovies = response.results;
+      });
   });
+ 
 
   describe("Top Rated Movies Page", () => {
     beforeEach(()=>{
@@ -28,6 +39,23 @@ describe("Base tests about movies lists", () => {
     it("displays the correct movie titles", () => {
       cy.get(".MuiCardHeader-content").each(($card, index) => {
         cy.wrap($card).find("p").contains(topRatedMovies[index].title);
+      });
+    });
+  });
+
+  describe("Upcoming Movies List", () => {
+    beforeEach(()=>{
+      cy.visit("/movies/upcoming")
+    })
+
+    it("displays the page header and 20 movies", () => {
+      cy.get("h3").contains("Upcoming Movies");
+      cy.get(".MuiCardHeader-root").should("have.length", 20);
+    });
+
+    it("displays the correct movie titles", () => {
+      cy.get(".MuiCardHeader-content").each(($card, index) => {
+        cy.wrap($card).find("p").contains(upComingMovies[index].title);
       });
     });
   });
