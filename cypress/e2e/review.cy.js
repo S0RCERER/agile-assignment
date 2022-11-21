bbbd59f97f8000e6784c8a41cdb6f9e435859069
Ling Feng <20099866@mail.wit.ie>
2022-11-21 23:28:25 +0000

    Test the elements on review form page

--- cypress/e2e/review.cy.js
+++ cypress/e2e/review.cy.js
@@ -16,7 +16,23 @@ describe("Review", () => {
           });
       });
     
-  describe("The Discover Movies page", () => {
+      before(() => {
+        cy.request(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+            "TMDB_KEY"
+          )}&language=en-US&include_adult=false&include_video=false&page=1`
+        )
+          .its("body")
+          .then((response) => {
+            movies = response.results;
+          });
+      });
+      beforeEach(() => {
+        cy.visit("/");
+      });
+
+
+  describe("Movies review page", () => {
     it("Should display a full review for this movie", () => {
         cy.visit(`/movies/${movies[1].id}`)
         cy.get(".MuiFab-root").contains("Reviews").click()
@@ -24,7 +40,22 @@ describe("Review", () => {
         cy.url().should("include", "/reviews/")
         
     });
-
   });
 
+  describe("Review Form Page", () => {
+    it("Should display the review form page with correct elements", () => {
+        cy.visit(`/`)
+        cy.get("button[aria-label='add to favorites']").eq(1).click();
+        cy.get("button").contains("Favorites").click();
+        cy.get(".MuiSvgIcon-root[data-testid='RateReviewIcon']").click()
+        cy.url().should("include", "/reviews/form")
+        cy.get("h2").contains("Write a review");
+        cy.get('.MuiButton-containedPrimary')
+        cy.get('.MuiButton-containedSecondary')
+        cy.get('#author')
+        cy.get('#review')
+        cy.get('#select-rating')
+    });
+});
+  
 });
